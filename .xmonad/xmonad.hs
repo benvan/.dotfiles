c1ea9e64ba259d8ef978f5e0fbec9d233d0f3fcd
Ben van Enckevort <benvan@gmail.com>
2012-06-23 21:55:02 +0100

    pause-key for new terminal

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -94,7 +94,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
  
     -- launch a terminal
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
- 
+    , ((0, xK_Pause), spawn $ XMonad.terminal conf)
     -- launch dmenu
     , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
  
